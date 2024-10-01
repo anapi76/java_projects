7927a59dae5d746ac019e11e4ae0ac8c0195a23c
ANAPIQUERASJIMENEZ <anpiji@floridauniversitaria.es>
2024-10-01 22:24:28 +0200

    actualizo datos

--- fizzbuzz/src/main/java/com/anapiqueras/App.java
+++ fizzbuzz/src/main/java/com/anapiqueras/App.java
@@ -1,11 +1,5 @@
 package com.anapiqueras;
 
-/* - Write a program that prints one line for each number from 1 to 100
-- Usually just print the number itself.
-- For multiples of three print Fizz instead of the number
-- For the multiples of five print Buzz instead of the number
-- For numbers which are multiples of both three and five print FizzBuzz instead of the number */
-
 import java.util.ArrayList;
 import java.util.List;
 
